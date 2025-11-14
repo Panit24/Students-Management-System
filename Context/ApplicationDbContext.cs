ecbe28d50d9b02ea176ac8dd3649895cb4dc3236
panit_suwimonsatein <lgwamhmmams@gmail.com>
2025-11-14 14:53:34 +0700

    Install Npgsql

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsManagementSystem.Models;
+using System;
+
+namespace StudentsManagementSystem.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,6 +1,25 @@
-﻿namespace StudentsManagementSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsManagementSystem.Models
 {
     public class Student
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName =>$"{FirstName} {MiddleName} {LastName}";
+
+        public string EmailAddress { get; set; }
+         
+        public int PhoneNumber { get; set; }
+
+        public string Gender { get; set; }
     }
 }
+ 
